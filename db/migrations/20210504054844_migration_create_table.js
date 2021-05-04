db6deaae70c3bbf683c84d2cb93c7594a45bbf73
Abhigyan <abhigyan.prakash@storms.com>
2021-05-04 15:30:05 +0800

    Adding db migration

--- db/migrations/20210504054844_migration_create_table.js
+++ db/migrations/20210504054844_migration_create_table.js
@@ -1,3 +1,27 @@
-exports.up = function (knex) {};
+export function up(knex) {
+  return knex.schema
+    .createTable('tigers', table => {
+      table.uuid('id').primary();
+      table.string('name');
+      table.date('date_of_birth');
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('tiger_sightings', table => {
+      table
+        .uuid('tiger_id')
+        .notNullable()
+        .references('id')
+        .inTable('tigers')
+        .onDelete('CASCADE');
+      table.timestamp('last_seen_at').defaultTo(knex.fn.now());
+      table.specificType('last_seen', 'Point');
+      table.string('image', 1000).nullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+}
 
-exports.down = function (knex) {};
+export function down(knex) {
+  return knex.schema.dropTable('tigers').dropTable('tiger_sightings');
+}
