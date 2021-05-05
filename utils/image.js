65e932cf71656d78dcd4be37cc1023777be3ed65
Abhigyan <abhigyan.prakash@storms.com>
2021-05-05 23:11:00 +0800

    Added image resizing functionality

--- utils/image.js
+++ utils/image.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import sharp from 'sharp';
 import { getConfig } from '../boot/config';
 
 export async function saveImage(context, image) {
@@ -18,17 +19,41 @@ export async function saveImage(context, image) {
 
     context.logger.debug(`Saving image to ${destPath}`);
     await fs.promises.rename(tempPath, destPath);
+
+    // Resize the image
+    resizeImage(context, destPath, image.name);
   } catch (err) {
-    context.logger.error(err, `Could not remove temp file`);
+    context.logger.error(err, `Could not save temp file`);
     throw err;
   }
 }
 
-export function getImagePath(context, imageName) {
+export function getImagePath(context, imageName, resized = true) {
   context.logger.debug(`Generating image path for image: ${imageName}`);
 
-  let uploadImagePath = getConfig('upload.image.path');
+  let uploadImagePath = resized
+    ? getConfig('upload.image.resize.path')
+    : getConfig('upload.image.path');
   let imagePath = `${uploadImagePath}/${imageName}`;
 
   return imagePath.replace('\\', '/');
+}
+
+function resizeImage(context, imagePath, imageName) {
+  context.logger.debug(`Resizing image: ${imageName}`);
+  let resizedDir = path.join(
+    __dirname,
+    '../',
+    getConfig('upload.image.resize.dir')
+  );
+
+  // Check if destination exists; if not create it
+  if (!fs.existsSync(resizedDir)) {
+    context.logger.debug(
+      `Destination directory does not exist: ${resizedDir}, creating it`
+    );
+    fs.mkdirSync(resizedDir, { recursive: true });
+  }
+
+  sharp(imagePath).resize(250, 200).toFile(path.join(resizedDir, imageName));
 }
